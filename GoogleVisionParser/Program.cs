40cc4fc82008a1b11002334bc7804cd93fd9b1fc
EA <Frost.qc@gmail.com>
2020-01-31 11:27:51 -0500

    -a

--- GoogleVisionParser/Program.cs
+++ GoogleVisionParser/Program.cs
@@ -10,14 +10,21 @@ namespace GoogleVisionParser
     {
         static void Main(string[] args)
         {
-            string _RawDataFromFile = File.ReadAllText("DATA.json");
-            NutritionnalParser _Parser = new NutritionnalParser();
-            var _Result = _Parser.Parse(_RawDataFromFile, out var _Report);
+            try
+            {
+                string _RawDataFromFile = File.ReadAllText("DATA.json");
+                NutritionnalParser _Parser = new NutritionnalParser();
+                var _Result = _Parser.Parse(_RawDataFromFile, out var _Report);
 
-            File.WriteAllText("Json.json", JsonConvert.SerializeObject(_Result));
+                File.WriteAllText("Json.json", JsonConvert.SerializeObject(_Result));
 
-            Console.WriteLine("Done.");
-            Console.ReadKey();
+                Console.WriteLine("Done.");
+                Console.ReadKey();
+            }
+            catch (Exception _Ex)
+            {
+               File.AppendAllText(Constants.ERROR_FILE, $"Error. Ex: [{_Ex.Message}], [{_Ex.StackTrace}], [{_Ex.InnerException?.Message}], [{_Ex.InnerException?.StackTrace}]");
+            }
         }
     }
 }
